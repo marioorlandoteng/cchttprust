3541be32f72dc97873c05cbc717a61d60dff186a
Mario Orlando <mario_orlando@live.com>
2025-01-17 12:51:44 +0700

    Respond with 200

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 #[allow(unused_imports)]
 use std::net::TcpListener;
+use std::io::Write;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
     
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
+                stream.write("HTTP/1.1 200 OK\r\n\r\n".as_bytes()).unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
